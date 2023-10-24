ad7d2357290d1f911d031aaca36190eefcd15e5a
heaseochung <heaseochung@gmail.com>
2023-10-24 05:28:41 +0000

    add file loaders for txt, json, pdf, docs and vectorizing with index

--- waveloGPT/macGPT.py
+++ waveloGPT/macGPT.py
@@ -1,32 +1,76 @@
 import os 
-from creds import apikey
-
+import datetime
+import pytesseract
 import streamlit as st 
+import json
+
+from creds import apikey
+from PIL import Image
+
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Chroma
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import DirectoryLoader
-from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.indexes import VectorstoreIndexCreator
-import pytesseract
-from PIL import Image
+from langchain.docstore.document import Document
+
+"""
+File Loaders
+"""
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders.pdf import PyPDFLoader
+from langchain.document_loaders.word_document import Docx2txtLoader
+from langchain.document_loaders.json_loader import JSONLoader
 
 
 
 os.environ['OPENAI_API_KEY'] = apikey
 image = Image.open('./waveloGPT/assets/maclogo.png')
 image = image.resize((600, 400))
+
 st.image(image, caption='mcmaster university')
 st.title('McMaster GPT')
-persistence = "./waveloGPT/store/"
+persistence = "./waveloGPT/store"
 embedding = OpenAIEmbeddings()
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+vectordb = Chroma(persist_directory=persistence, embedding_function=embedding)
+
+# File uploader
+up_file = st.file_uploader("Upload your File")
+if up_file:
+    target_dir = os.path.join("./waveloGPT/loader", str(datetime.date.today()))
+    os.makedirs(target_dir, exist_ok=True)
+    target_name = up_file.name
+    target_path = os.path.join(target_dir, target_name)
+    
+    with open(target_path, 'wb') as f:
+        f.write(up_file.getvalue())
+
+    documents = None
+    if up_file.type.endswith("plain"):
+        documents = TextLoader(target_path).load()
+    elif up_file.type.endswith("json"):
+        documents = JSONLoader(
+            file_path=target_path,
+            jq_schema='.',
+            text_content=False
+        ).load()
+    elif up_file.type.endswith("pdf"):
+        documents = PyPDFLoader(target_path).load()
+    elif up_file.type.endswith("wordprocessingml.document"):
+        documents = Docx2txtLoader(target_path).load()
+
+    if documents:
+        index = VectorstoreIndexCreator().from_documents(documents)
+    else:
+        raise ValueError("The file is not supported.")
+
 
 #prompt handling
 prompt = st.text_input('Plug in your prompt here') 
@@ -41,47 +85,25 @@ prompt_template = PromptTemplate(
     Helpful Answer:
     """
 )
-
-
-
-
-
-
-
-
-if persistence and os.path.exists(persistence):
-    vectordb = Chroma(persist_directory=persistence, embedding_function=embedding)
-    index = VectorStoreIndexWrapper(vectorstore=vectordb) 
-else:
-    text_loader_kwargs={'autodetect_encoding': True}
-    loader = DirectoryLoader('./waveloGPT/loader', glob="**/*.txt",loader_cls=TextLoader,loader_kwargs=text_loader_kwargs)
-
-    documents = loader.load()
-    if persistence:
-        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"./waveloGPT/store/"}).from_documents(documents)
-    else:
-        index = VectorstoreIndexCreator().from_documents(documents)
-    vectordb = Chroma(persist_directory=persistence, embedding_function=embedding)
-    index = VectorStoreIndexWrapper(vectorstore=vectordb) 
-
  
-chain = RetrievalQA.from_chain_type(
-  llm=ChatOpenAI(model="gpt-3.5-turbo-16k-0613"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
-
 #chain.combine_documents_chain.llm_chain.prompt = prompt_template
-
 # prompt = "what do you know about Kafka Multi Tenant notes?"
 # prompt = "what do you know about ISOS, and can you walk me through everything you know?"
 # prompt = "How do you rebuild ISOS services to AWS ECR?"
 #prompt = "How do I provision nomad on EC2"
-if prompt:
+
+if up_file and prompt:
+    chain = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(model="gpt-3.5-turbo-16k-0613"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+        )
+    
     st.write(chain.run(prompt))
 
     with st.expander('Document Similarity Search'):
         # Find the relevant pages
         search = vectordb.similarity_search_with_score(prompt) 
+        print(search)
         # Write out the first 
-        st.write(search[0][0].page_content)
-        st.write(f"source from:{search[0][0].metadata['source']}")
+        # st.write(search[0][0].page_content)
+        # st.write(f"source from:{search[0][0].metadata['source']}")
